e90c8509f7ae4f67d068e2e908df6c166e2a9a70
radio-alice <zacshermann@gmail.com>
2018-08-30 00:46:04 -0500

    better
    added planet colors, balanced forces

--- planets/Assets/Scripts/Planet.cs
+++ planets/Assets/Scripts/Planet.cs
@@ -5,13 +5,16 @@ using UnityEngine;
 [RequireComponent(typeof(Rigidbody))]
 public class Planet : MonoBehaviour {
     public float G = 10; //quickly modify gravitational strength
-    public float forceScalar = 100; //quickly scale orbital force 
+    public float forceScalar = 25; //quickly scale orbital force 
     public bool released; //whether or not plane has been released and is affected by forces
     public Transform star; //the star to orbit
 
     Rigidbody getRigidbody; 
     float mass; //mass of planet (taken from rigidbody)
     SphereCollider getCollider;
+    TrailRenderer getTrail;
+    Material material;
+
     float t = 0f; //counter for size scaling
     float starMass;
 
@@ -25,30 +28,36 @@ public class Planet : MonoBehaviour {
         mass = getRigidbody.mass;
 
         getCollider = GetComponent<SphereCollider>();
+        getTrail = GetComponent<TrailRenderer>();
+        material = GetComponent<MeshRenderer>().material;
         }
 
     void OnEnable()
 	{
         getCollider.enabled = false; //disable collisions until released
-        released = false;
-        getRigidbody.constraints = RigidbodyConstraints.FreezeAll;
-        ScaleSize(t);
+        released = false; //tell script planet is not released
+        getRigidbody.constraints = RigidbodyConstraints.FreezeAll; //freeze movement
+        getTrail.Clear(); //clear previous trail
+
+        material.color = Random.ColorHSV(0, 1, .2f, 1, .3f, 1, 1, 1);//random color
+        ScaleSize(t); //scale up and down (so there's no blip when scale size kicks in in Update())
 	}
 
-	void Update () {
-        if (released)
+	void Update()
+    {
+        if (!released)
         {
-            UseGravity(); //interact w stars' gravity once spawn script says to (upon release of planet)
-            getCollider.enabled = true;
-            t = 0f; //reset t
-        } else {
             t += Time.deltaTime; //increment t every frame
             ScaleSize(t); //scale size up and down
         }
+    }
 
+	void FixedUpdate()
+	{
+        if (released) UseGravity(); //interact w stars' gravity once spawn script says to (upon release of planet)
 	}
 
-    void UseGravity(){
+	void UseGravity(){
         float distance; //distance to star
         Vector3 direction; //direction to star
 
@@ -63,7 +72,9 @@ public class Planet : MonoBehaviour {
 
     public void AddOrbitalForce(float dragDistance, Vector2 direction) //ensures orbiting, instead of just falling towards star
     {
-        float force = dragDistance * forceScalar * mass; //scale force by finger drag, mass
+        float starDistance = (transform.position - star.transform.position).magnitude;
+        float force = dragDistance * forceScalar * Mathf.Sqrt(mass) * 1.87f/Mathf.Sqrt(starDistance); //scale force by finger drag, mass, distance to star
+
         Vector3 starDirection = (transform.position - star.transform.position).normalized; //get star direction
         Vector3 starTangent = Vector3.Cross(starDirection, Vector3.up); //find tangent to surface of star
         if (Mathf.Approximately(starTangent.sqrMagnitude, 0f)) starTangent = Vector3.Cross(Vector3.up, starDirection); //check if tangent plane is parallel to star and find another tangent if so
@@ -73,6 +84,8 @@ public class Planet : MonoBehaviour {
         getRigidbody.constraints = RigidbodyConstraints.None;
         getRigidbody.AddForce(force * inputTangent.normalized); //shoot planet along chosen tangent (causes orbit if all our gravity, force values are balanced)
         released = true;
+        getCollider.enabled = true;
+        t = 0f; //reset t
     }
 
     void OnCollisionEnter(Collision collision)
@@ -86,7 +99,7 @@ public class Planet : MonoBehaviour {
 	void ScaleSize(float counter)
 	{
         transform.localScale = (Vector3.one + (Vector3.one * (Mathf.Sin(counter - Mathf.PI / 2)) * .6f)) * .4f; //scale up and down starting small
-        mass = Mathf.Pow((transform.localScale.x / 2), 2f) * 15; //adjust mass with volume
+        mass = Mathf.Pow((transform.localScale.x / 2), 2f) * 10; //adjust mass with volume
     }
 }
 
